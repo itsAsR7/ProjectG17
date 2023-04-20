81c918463e25633ec3a1f16f78ee3c0ca66df930
Wiwat Thaphon <wiwat.thaphon@gmail.com>
2023-04-19 21:00:08 -0400

    Bottom Navigation Screen

--- app/src/main/java/com/example/projectg17/MainActivity.kt
+++ app/src/main/java/com/example/projectg17/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.projectg17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // setup the bottom navigation menu so it responds to clicks
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container1) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(
+            findViewById<BottomNavigationView>(R.id.bottomNavigationView),
+            navController
+        )
     }
 }
---
+++ app/src/main/java/com/example/projectg17/ScreenAFragment.kt
@@ -0,0 +1,12 @@
+package com.example.projectg17
+
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class ScreenAFragment : Fragment(R.layout.fragment_screen_a) {
+
+
+}
